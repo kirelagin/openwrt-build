7b89ed217106ddc8e4f92eb7a2d4839c3aa7cac1
Kirill Elagin <kirelagin@gmail.com>
2017-07-16 00:55:36 +0300

    Make configs location configurable

--- make.sh
+++ make.sh
@@ -2,13 +2,14 @@
 
 openwrt_version="15.05.1"
 builder_dir="OpenWrt-ImageBuilder-$openwrt_version-ar71xx-generic.Linux-x86_64"
+configs_dir="./configs"
 
 host="$1"
 [ -n "$host" ] || { echo "No host selected"; exit 1; }
 
-host_profile_file="configs/$host/profile"
-host_pkgs_file="configs/$host/pkgs"
-host_files_dir="configs/$host/files"
+host_profile_file="$configs_dir/$host/profile"
+host_pkgs_file="$configs_dir/$host/pkgs"
+host_files_dir="$configs_dir/$host/files"
 
 [ -f "$host_profile_file" -a -f "$host_pkgs_file" -a -d "$host_files_dir" ] || { echo "Invalid host configurateion"; exit 2; }
 
