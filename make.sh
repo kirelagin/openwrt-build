9ac9c8e19ceca136a329697edcfc10790e6d4c33
Kirill Elagin <kirelagin@gmail.com>
2018-10-26 12:07:37 +0100

    tmp

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openwrt_version="17.01.4"
+openwrt_version="18.06.1"
 configs_dir="./configs"
 
 host="$1"
@@ -18,10 +18,17 @@ host_files_dir="$configs_dir/$host/files"
 target=$(cat "$host_target_file" | head -n 1)
 profile=$(cat "$host_target_file" | tail -n +2 | tail -n 1)
 
-builder_dir="lede-imagebuilder-$openwrt_version-$target.Linux-x86_64"
-[ -d "$builder_dir" ] || { echo "Image generator is missing"; exit 3; }
+builder_dir="$TMPDIR/openwrt-imagebuilder-$openwrt_version-$target.Linux-x86_64"
+if [ ! -d "$builder_dir" ]; then
+  generator_url="https://downloads.openwrt.org/releases/$openwrt_version/targets/${target/-/\/}/openwrt-imagebuilder-$openwrt_version-$target.Linux-x86_64.tar.xz"
+  echo "Image generator is missing, downloading it from $generator_url"
+  #curl -f -C - -o "$builder_dir".tar.xz "$generator_url" || { echo "Could not download the generator"; exit 2; }
+  tar -xf "$builder_dir.tar.xz" -C "$TMPDIR" || { echo "Could not extract the generator"; exit 2; }
+fi
+[ -d "$builder_dir" ] || { echo "AAAAAAAAAAAAAAA"; exit 2; }
+
 out_dir="$(readlink -f "$builder_dir/bin/$host")"
-img_file="$builder_dir/bin/$host/lede-$openwrt_version-$target-$profile-squashfs-sysupgrade.bin"
+img_file="$builder_dir/bin/$host/openwrt-$openwrt_version-$target-$profile-squashfs-sysupgrade.bin"
 
 rm -f "$img_file"
 make -C "$builder_dir"  image BIN_DIR="$out_dir" PROFILE="$profile" PACKAGES="$(cat "$host_pkgs_file" | tr '\n' ' ')" FILES="$(readlink -f "$host_files_dir")/"
